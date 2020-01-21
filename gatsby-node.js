8869d6c3569232e82adeea0e31fa04c1af9bb147
Brent Abruzese <brent.abruzese@gmail.com>
2020-01-21 11:31:19 -0500

    commented out gatsby-node for deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,23 @@
-exports.createPages = async function({ actions, graphql }) {
-  const { data } = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-  data.allMarkDownRemark.edges.forEach(edge => {
-    const slug = edge.node.fields.slug
-    actions.createPage({
-      path: slug,
-      component: require.resolve(`./src/templates/aws-temp.js`),
-      context: { slug: slug },
-    })
-  })
-}
+// exports.createPages = async function({ actions, graphql }) {
+//   const { data } = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   data.allMarkDownRemark.edges.forEach(edge => {
+//     const slug = edge.node.fields.slug
+//     actions.createPage({
+//       path: slug,
+//       component: require.resolve(`./src/templates/aws-temp.js`),
+//       context: { slug: slug },
+//     })
+//   })
+// }
