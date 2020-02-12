1a676080268c75d68cd437865238a4e49a09db5f
Brent Abruzese <brent.abruzese@gmail.com>
2020-02-12 10:14:40 -0500

    removed date field from yaml

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,10 @@ module.exports = {
       }
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/app/*`] },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
